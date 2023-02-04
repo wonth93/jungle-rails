56bface05b3f0ad4b8788ccd1e2e6365a7fa1595
wonth93 <wongth93@gmail.com>
2023-02-04 23:36:01 +0000

    update admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,11 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
+  # http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
   
   def show
+    @product = Product.all
+    @product_total = Product.sum(:quantity)
+    @category = Category.all
+    @category_total = Category.count
   end
   
 end
