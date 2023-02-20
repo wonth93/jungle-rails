5f1dcb1f00a062eb650e3851092cee1f7073c770
wonth93 <wongth93@gmail.com>
2023-02-20 06:01:51 +0000

    add home page cypress test

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+describe("Navigation", function() {
+  
+  it("should visit home page", () => {
+    cy.visit("/");
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+});
